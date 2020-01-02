b3ea88e2bc2f85421bffa030839777698d9f9fa2
Reese M Bowman <reesemb1@gmail.com>
2020-01-02 17:40:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,20 +1,47 @@
+require 'pry'
+# binding.pry
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  index =  0 
+  item_hash = {}
+  while index < collection.length do 
+    if name == collection[index][:item]
+      return collection[index]
+    end
+      index += 1 
+  end
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  index = 0 
+  new_cart = []
+  while index < cart.length do 
+    new_cart_item = find_item_by_name_in_collection(cart[index][:item], new_cart)
+    
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+
+  index = 0 
+  while index < cart.length do
+    coup_name = coupons[0][:item]
+    disc_value = coupons[0][:cost] / coupons[0][:num]
+    item_name = cart[index][:item]
+    if item_name == coup_name
+      if cart[index][:count] % coupons[0][:num] === 0 
+        cart[index][:item] = item_name + " W/COUPON"
+        cart[index][:price] = disc_value
+      end
+    end
+    index += 1 
+  end
+#binding.pry
 end
 
 def apply_clearance(cart)
